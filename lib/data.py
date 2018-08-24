26dbb6c3d79ec1d42e1651b09a995014a6fa2668
mimirobo <m13.esmaeili@gmail.com>
2018-08-24 20:06:20 +0430

    Load All Custom Dataset in grayscale mode

--- lib/data.py
+++ lib/data.py
@@ -148,6 +148,7 @@ def load_data(opt):
         shuffle = {'train': True, 'test': True}
         transform = transforms.Compose([transforms.Scale(opt.isize),
                                         transforms.CenterCrop(opt.isize),
+                                        transforms.Grayscale(),
                                         transforms.ToTensor(),
                                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), ])
 
