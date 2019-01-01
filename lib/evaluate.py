fd611de0f4265c1035e33cf99259dda7a89d2856
Mimi <m13.esmaeili@gmail.com>
2019-01-01 14:47:11 +0330

    to cpu

--- lib/evaluate.py
+++ lib/evaluate.py
@@ -45,7 +45,7 @@ def roc(labels, scores, saveto=None):
     roc_auc = dict()
 
     # True/False Positive Rates.
-    fpr, tpr, _ = roc_curve(labels.numpy(), scores.numpy())
+    fpr, tpr, _ = roc_curve(labels.cpu(), scores.cpu())
     
     #print("FPR:{}".format(fpr))
     #print("TPR:{}".format(tpr))
